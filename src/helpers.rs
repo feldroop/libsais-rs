77aee6a9eaed898ea0d7cba73c3c1dfc0b5053fd
Felix-Droop <felix.droop@fu-berlin.de>
2025-08-11 20:45:56 +0200

    rename function

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,7 @@
 /// TODO doc
-pub fn concatenate_strings<'a>(iter: impl IntoIterator<Item = &'a [u8]>) -> Vec<u8> {
+pub fn concatenate_strings_for_generalized_suffix_array<'a>(
+    iter: impl IntoIterator<Item = &'a [u8]>,
+) -> Vec<u8> {
     let strings: Vec<_> = iter.into_iter().collect();
     let needed_capacity = strings.iter().map(|&s| s.len()).sum::<usize>() + strings.len();
     let mut concatenated_string = Vec::with_capacity(needed_capacity);
