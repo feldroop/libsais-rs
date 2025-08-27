ff8169bac0770dbac9de7beeb4834888bac0ddb6
Felix-Droop <felix.droop@fu-berlin.de>
2025-08-27 12:12:49 +0200

    Add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,88 @@
+/*!
+ * An idiomatic and mostly safe API wrapper for the awesome and _very_ fast C library
+ * [`libsais`] by Ilya Grebnov.
+ *
+ * [`libsais`] provides highly optimized implementation for the features listed below.
+ * These implementations are widely used in the analysis of very large sequences, such as genome analysis
+ * in bioinformatics. For example, [`libsais`] contains the fastest suffix array construction
+ * implementation to date. [Comparison to `libdivsufsort`] | [Benchmark crates.io]
+ *
+ * <div class="warning">
+ *
+ * This crate is not yet battle-tested, there might be bugs. The API is still subject to small changes.
+ * Any kind of feedback and suggestions via the issue tracker is highly appreciated!
+ *
+ * </div>
+ *
+ * ## Features
+ *
+ * This crate exposes the whole functionality of [`libsais`].
+ * It might be useful to also check out the [documentation of the original library].
+ * For further details on the individual features, please refer to the module-level documentation.
+ *
+ * * [suffix_array]: Construct suffix arrays for `u8`/`u16`/`i32`/`i64` input texts and `i32`/`i64` output texts,
+ *   with support for generalized suffix arrays
+ * * [bwt]: Construct the Burrows-Wheeler-Transform (BWT) for `u8`/`u16` texts
+ * * [unbwt]: Recover the original text from a BWT
+ * * [plcp]: Construct the permuted longest common prefix array (PLCP) for a suffix array and text
+ * * [lcp]: Construct the longest common prefix array from a PLCP for a suffix array and text
+ * * [context]: Use a memory allocation optimization for repeated calls on small inputs
+ *
+ * ## Usage
+ *
+ * This crate provides generic builder-like APIs for all of the features listed above.
+ * The following is a simple example of how to use this library to construct a suffix array in parallel:
+ *
+ * ```rust
+ * use libsais::{SuffixArrayConstruction, ThreadCount};
+ *
+ * let text = b"barnabasbabblesaboutbananas";
+ * let suffix_array: Vec<i32> = SuffixArrayConstruction::for_text(text)
+ *     .in_owned_buffer()
+ *     .multi_threaded(ThreadCount::openmp_default())
+ *     .run()
+ *     .expect("The example on the front page should really work")
+ *     .into_vec();
+ * ```
+ *
+ * ## Examples
+ *
+ * There are [examples] for multiple non-trivial use-cases of this library.
+ *
+ * ## Multithreading
+ *
+ * This library supports the multithreaded implementations of [`libsais`] via the optional `openmp` feature,
+ * which is enabled by default.
+ *
+ * [`libsais`]: https://github.com/IlyaGrebnov/libsais
+ * [Comparison to `libdivsufsort`]: https://github.com/IlyaGrebnov/libsais/blob/master/Benchmarks.md
+ * [Benchmark crates.io]: https://github.com/feldroop/benchmark_crates_io_sacas
+ * [documentation of the original library]: https://github.com/IlyaGrebnov/libsais
+ * [examples]: https://github.com/feldroop/libsais-rs/tree/master/examples
+ */
+
+/// Construct a Burrows-Wheeler-Transform
 pub mod bwt;
+
+/// Use an optimization for repeated calls of `libsais` functions on small inputs
 pub mod context;
+
+/// The error type of this crate
 pub mod error;
+
+/// Construct the permuted longest common prefix array
 pub mod lcp;
+
+/// Construct the permuted longest common prefix array
 pub mod plcp;
+
+/// Construct a (generalized) suffix array
 pub mod suffix_array;
+
+/// Typestate for builder APIs, ikely not relevant for you
 pub mod type_state;
+
+/// Recover the text from a Burrows-Wheeler-Transform
 pub mod unbwt;
 
 mod generics_dispatch;
@@ -17,15 +95,16 @@ use generics_dispatch::{
 use sealed::Sealed;
 use type_state::OutputElementOrUndecided;
 
-/// The version of the C library libsais wrapped by this crate
+/// The version of the original C library `libsais` wrapped by this crate
 pub use libsais_sys::libsais::LIBSAIS_VERSION_STRING;
 
-/// The maximum text size that this library can handle when using i32-based buffers
+/// The maximum text size that this library can handle when using `i32`-based buffers
 pub const LIBSAIS_I32_OUTPUT_MAXIMUM_SIZE: usize = 2147483647;
 
-/// The maximum text size that this library can handle when using i64-based buffers
+/// The maximum text size that this library can handle when using `i64`-based buffers
 pub const LIBSAIS_I64_OUTPUT_MAXIMUM_SIZE: usize = 9223372036854775807;
 
+// Main entry points of the library
 #[doc(inline)]
 pub use {
     bwt::BwtConstruction, lcp::LcpConstruction, plcp::PlcpConstruction,
@@ -34,20 +113,18 @@ pub use {
 
 // TODOs:
 
-//      sort buffermode typestate according to importance always
-
 //      some comments in code
 //      good docs functions
 
 //      good docs module and crate level
-//      examples
 
 //      setup CI and such, also without default features, clippy, etc.
 //      release-plz good release (also libsais-sys update)
-//      update benchmark code
 
+/// Possible element types of input texts and output data structures storing text elements implement this trait.
+/// You cannot implement it and don't need to.
 pub trait InputElement:
-    Sealed + std::fmt::Debug + Copy + TryFrom<usize, Error: std::fmt::Debug> + Into<i64> + Clone + Ord
+    Sealed + std::fmt::Debug + Copy + Clone + Ord + TryFrom<usize, Error: std::fmt::Debug> + Into<i64>
 {
     const RECOMMENDED_EXTRA_SPACE: usize;
     const ZERO: Self;
@@ -57,14 +134,16 @@ pub trait InputElement:
     type MultiThreadedOutputDispatcher<O: OutputElementOrUndecided>: OutputDispatch<Self, O>;
 }
 
+/// Possible element types of output data structures storing indices implement this trait.
+/// You cannot implement it and don't need to.
 pub trait OutputElement:
     Sealed
     + std::fmt::Debug
+    + std::fmt::Display
     + Copy
+    + Clone
     + TryFrom<usize, Error: std::fmt::Debug>
     + Into<i64>
-    + Clone
-    + std::fmt::Display
 {
     const MAX: Self;
     const ZERO: Self;
@@ -92,12 +171,15 @@ pub trait OutputElement:
         + LibsaisLcpFunctions<i64, Self>;
 }
 
+/// Certain features such as the [Burrows-Wheeler-Transform](bwt) are only available for `u8`/`u16`-based texts.
 pub trait SmallAlphabet: InputElement {
     const FREQUENCY_TABLE_SIZE: usize;
 }
 
+/// When using `i32`/`i64`-based texts, the API for suffix array construction changes slightly. [Details](TODO)
 pub trait LargeAlphabet: InputElement + OutputElement {}
 
+/// A wrapper type for passing the desired number of threads. Only useful when the `openmp` feature is activated.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ThreadCount {
     pub(crate) value: u16,
